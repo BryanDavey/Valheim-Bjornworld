69e9c71ef0359e43b5e074c1e09ba3881d7a0f8d
BryanDavey <85777055+BryanDavey@users.noreply.github.com>
2025-10-10 08:36:02 -0400

    Add checkForUpdates.ps1 to setup .bat script

--- setup_bypassExecutionPolicy.bat
+++ setup_bypassExecutionPolicy.bat
@@ -1,3 +1,4 @@
 @echo off
 powershell -ExecutionPolicy Bypass -File "%~dp0setup.ps1"
+powershell -ExecutionPolicy Bypass -File "%~dp0checkForUpdates.ps1"
 powershell -ExecutionPolicy Bypass -File "%~dp0CreateShortcut.ps1"
