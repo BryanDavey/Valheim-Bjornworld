c0f7d651394a636b862cd32890c5f777f0e4b9e6
BryanDavey <85777055+BryanDavey@users.noreply.github.com>
2025-10-10 08:38:01 -0400

    Add NextCloudURLs.ps1 to track Nextcloud download links

---
+++ NextCloudURLs.ps1
@@ -0,0 +1,3 @@
+# Nextcloud file download links for Bjornworld
+$remoteVersionUrl = "https://download.brydavey.com/s/n7qG3KwbdKRgAKR/download"
+$modsUrl = "https://download.brydavey.com/s/5MjXxgXWAWqBz4L/download"
