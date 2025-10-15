3837aefb64ed87902a07c6b7f969c94b3cec2844
BryanDavey <85777055+BryanDavey@users.noreply.github.com>
2025-10-14 22:44:18 -0400

    Add BepInExUrl to NextCloudURLs.ps1

--- NextCloudURLs.ps1
+++ NextCloudURLs.ps1
@@ -1,3 +1,4 @@
 # Nextcloud file download links for Bjornworld
 $remoteVersionUrl = "https://download.brydavey.com/s/n7qG3KwbdKRgAKR/download"
 $modsUrl = "https://download.brydavey.com/s/5MjXxgXWAWqBz4L/download"
+$BepInExUrl = "https://download.brydavey.com/s/4bLwHYP6e9MyqXP/download"
